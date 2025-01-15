917e190fca6851fa7e397a846baee078dba00452
Aniol Rodriguez <arodrigue96@gmail.com>
2025-01-15 15:26:59 +0100

    Add test cases for Header component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["src/setupTest.ts"],
+    coverage: {
+      include: ["src/**/*.tsx", "src/**/*.ts"],
+      exclude: [
+        "src/main.tsx",
+        "src/vite-env.d.ts",
+        "src/**/*.test.tsx",
+        "src/**/*.test.ts",
+        "src/**/types.ts",
+      ],
+      reporter: ["lcov", "text"],
+    },
+  },
+});
